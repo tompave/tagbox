903a31f5fccaa586427bb3dfcbf94e592ae43abf
tom <tommaso@pavese.info>
2013-09-05 22:37:57 +0100

    Updated script to use only the params array field rather than the chained string

--- scripts/tag_box.js
+++ scripts/tag_box.js
@@ -1,13 +1,7 @@
 $(function(){
-
-    // var jq_tagsFormInput = $("#tag_list");
-    // var jq_tagBox = $("#tag_box");
-    // var jq_hiddenInput = $("#hidden_input");
-
     setupSimulatedFocus();
-    setupInputKeyPressHandling();
+    setupTypeTargetKeyPressHandling();
     setupDimissTagListener();
-
 });
 
 
@@ -19,16 +13,16 @@ $(function(){
  */
 function setupSimulatedFocus(){
     var jq_tagBox = $("#tag_box");
-    var jq_hiddenInput = $("#hidden_input");
+    var jq_typeTarget = $("#type_target");
 
     jq_tagBox.click(function(event){
-        jq_hiddenInput.focus();
+        jq_typeTarget.focus();
     });
 
-    jq_hiddenInput.focus(function(event){
+    jq_typeTarget.focus(function(event){
         jq_tagBox.addClass("has_focus");
     });
-    jq_hiddenInput.blur(function(event){
+    jq_typeTarget.blur(function(event){
         jq_tagBox.removeClass("has_focus");
     });
 }
@@ -40,24 +34,24 @@ function setupSimulatedFocus(){
 /**
  *
  */
-function setupInputKeyPressHandling(){
-    var jq_hiddenInput = $("#hidden_input");
+function setupTypeTargetKeyPressHandling(){
+    var jq_typeTarget = $("#type_target");
 
     //handle input: create tag if key==(enter, tab, comma, space), adjust width
-    jq_hiddenInput.keydown(function(event){
+    jq_typeTarget.keydown(function(event){
         var key = event.which
         if (key === 13 || key === 9 || key === 44 || key === 188 || key === 32){
             event.preventDefault();
             verifyAndCreateTag();
         }
         else
-            adjustHiddenInputWidth();
+            adjustTypeTargetWidth();
     });
 
     //adjust width on keyup as well, to cover any case (crossbrowser)
     //also, fires the ajax request
-    jq_hiddenInput.keyup(function(event){
-        adjustHiddenInputWidth();
+    jq_typeTarget.keyup(function(event){
+        adjustTypeTargetWidth();
         //handleAutocompletionRemote();        
     });
 }
@@ -74,7 +68,7 @@ function setupDimissTagListener() {
                      null,
                      function(event){
                         event.preventDefault();
-                        removeTag($(this).parent());
+                        removeTagElementAndHiddenInput($(this).parent());
                      });
 }
 
@@ -88,8 +82,8 @@ function setupDimissTagListener() {
  *
  */
 function handleAutocompletionRemote(){
-    var jq_hiddenInput = $("#hidden_input");
-    var text = jq_hiddenInput.val();
+    var jq_typeTarget = $("#type_target");
+    var text = jq_typeTarget.val();
 
     if(text.length > 2) {
         var api_url = window.location.protocol + "//" +
@@ -106,16 +100,16 @@ function handleAutocompletionRemote(){
 
 
 /**
- *  encapsulates logic to adjust the hidden input's width
+ *  to adjust the hidden input's width
  */
-function adjustHiddenInputWidth() {
-    var jq_hiddenInput = $("#hidden_input");
-    var len = jq_hiddenInput.val().length;
+function adjustTypeTargetWidth() {
+    var jq_typeTarget = $("#type_target");
+    var len = jq_typeTarget.val().length;
 
-    jq_hiddenInput.css("width", len*10);
+    jq_typeTarget.css("width", len*10);
 
-    if(parseInt(jq_hiddenInput.css("width")) < 40){
-        jq_hiddenInput.css("width", "40px")
+    if(parseInt(jq_typeTarget.css("width")) < 40){
+        jq_typeTarget.css("width", "40px")
     }
 }
 
@@ -123,14 +117,14 @@ function adjustHiddenInputWidth() {
 
 
 /**
- *  decides whether to create a tag and fires the process
+ *  if something has been typed in the input, create a new tag
  */
 function verifyAndCreateTag() {
-    var jq_hiddenInput = $("#hidden_input");
-    var text = jq_hiddenInput.val();
+    var jq_typeTarget = $("#type_target");
+    var text = jq_typeTarget.val();
 
     if(text.length > 0)
-        insertTag(text);
+        insertTagElement(text);
 }
 
 
@@ -139,33 +133,36 @@ function verifyAndCreateTag() {
 /**
  *  
  */
-function insertTag(text) {
-    //var jq_tagsFormInput = $("#tag_list");
-    var jq_hiddenInput = $("#hidden_input");
+function insertTagElement(text) {
+    var jq_typeTarget = $("#type_target");
 
-    var jq_newTag = $(tagHtmlString(text));
-    jq_newTag.insertBefore(jq_hiddenInput);
+    var jq_newTag = $(buildTagHtmlString(text));
+    jq_newTag.insertBefore(jq_typeTarget);
 
     // resetting the input
-    jq_hiddenInput.val("").css("width", "60px");
+    jq_typeTarget.val("").css("width", "60px");
  
-
-        //adds the skill string to the hidden input in the form.
-        //I'm using "$%" to separate entries. I'll parse and split the string server-side
-    // var new_value = jq_tagsFormInput.val() + text + "$%";
-    // jq_tagsFormInput.val(new_value);
+    insertTagHiddenField(text);
+}
 
 
-    jq_newField = $(hiddenInputHtmlString(text));
+/**
+ *  
+ */
+function insertTagHiddenField(text) {
+    jq_newField = $(buildHiddenInputHtmlString(text));
     $("#the_form").prepend(jq_newField);
 }
 
 
 
+
+
+
 /**
- *  builds the html string for the tag
+ *  builds the html string for the tag element
  */
-function tagHtmlString(text) {
+function buildTagHtmlString(text) {
     var tag_str = "<div class='tag_element'><span class='tag_label'>" + text +
         "</span><a href='#' title='remove' class='tag_dismiss'>&times;</a></div>";
     return tag_str;
@@ -175,7 +172,7 @@ function tagHtmlString(text) {
 /**
  *  builds the html string for a hidden form input
  */
-function hiddenInputHtmlString(text) {
+function buildHiddenInputHtmlString(text) {
     var input_str =  "<input id='" + text + "' name='tag_list[]' type='hidden' value='" + text + "'>";
     return input_str;
 }
@@ -185,19 +182,37 @@ function hiddenInputHtmlString(text) {
 /**
  *  
  */
-function removeTag(jq_tag){
-    //var jq_tagsFormInput = $("#tag_list");
+function removeTagElementAndHiddenInput(jq_tag){
     var tagName = jq_tag.find(".tag_label").html();
-
     // remove the tag from the tag box
     jq_tag.remove();
-
-    // remove the tag from the hidden form input
-    // var originalValue = jq_tagsFormInput.val();
-    // var pattern = tagName + "$%";
-    // var newValue = originalValue.replace(pattern, "");
-    // jq_tagsFormInput.val(newValue);
-
+    // remove the hidden input
     $("input#"+tagName).remove();
 }
 
+
+
+
+// -- DEPRECATED
+
+/**
+ *  adds the tag string to the "chained" hidden input (if used)
+ *  I'm using "$%" to separate entries. I'll parse and split the string server-side
+ */
+// function addNewTagToHiddenStringField(text) {
+//     var jq_tagsFormInput = $("#tag_list");
+//     var new_value = jq_tagsFormInput.val() + text + "$%";
+//     jq_tagsFormInput.val(new_value);
+// }
+
+
+/**
+ *  removes the tag string from the "chained" hidden input (if used)
+ */
+ // function removeDeletedTagFromHiddenStringField(text) {
+ //    var jq_tagsFormInput = $("#tag_list");
+ //    var originalValue = jq_tagsFormInput.val();
+ //    var pattern = text + "$%";
+ //    var newValue = originalValue.replace(pattern, "");
+ //    jq_tagsFormInput.val(newValue);
+ // }
