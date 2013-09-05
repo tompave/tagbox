58d1a8f84d7e89afb30dcd0673faa6f3ade84edd
tom <tommaso@pavese.info>
2013-09-05 23:58:25 +0100

    Added code live preview!

---
+++ scripts/live_code.js
@@ -0,0 +1,23 @@
+$(function(e) {
+    updatePreview();
+
+
+    $("#type_target").keyup(function(event){
+        updatePreview();
+    });
+});
+
+
+function updatePreview() {
+    var jq_liveCodePreview = $("#live_code_preview");
+    var jq_formContainer = $("#form_container");
+
+    var html_code = jq_formContainer.html();
+    var escapedString = html_code.replace(/<input id/g, "\n\t&lt;input id");
+
+    escapedString = escapedString.replace(/<div class=\"tag_element/g,'\n\t&lt;div class="tag_element');
+    escapedString = escapedString.replace(/</g,"&lt;").replace(/>/g,"&gt;");
+
+
+    jq_liveCodePreview.html(escapedString);
+}
