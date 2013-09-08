36acace9c59da914727fd7114ed347bc33b49ee1
tom <tommaso@pavese.info>
2013-09-08 19:40:19 +0200

    added notes to the accessory js files

--- scripts/live_code.js
+++ scripts/live_code.js
@@ -1,3 +1,12 @@
+/**
+
+This code is used in the example to display a preview of the html source code.
+It is NOT related to the tagbox jQuery plugin and is not necessary in production.
+
+Copyright (c) 2013 Tommaso Pavese (tommaso@pavese.info)
+
+*/
+
 $(function(e) {
     updatePreview();
 
--- scripts/logic.js
+++ scripts/logic.js
@@ -1,3 +1,12 @@
+/**
+
+This code is used in the example.
+It is NOT related to the tagbox jQuery plugin and is not necessary in production.
+
+Copyright (c) 2013 Tommaso Pavese (tommaso@pavese.info)
+
+*/
+
 $(document).ready(function(e){
 
     $("#tag_box").tagbox();
