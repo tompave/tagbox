3fbd4a902f42b8e140d0bfe5fcf6043337fd13d2
tom <tommaso@pavese.info>
2013-09-08 02:54:45 +0200

    successufully turned the tag logic into a JQuery plugin (i think)

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,3 +1,5 @@
 $(document).ready(function(e){
 
+    $("#tag_box").tag_box();
+
 });
--- scripts/tag_box.js
+++ scripts/tag_box.js
@@ -1,173 +1,224 @@
-$(function(){
-    setupSimulatedFocus();
-    setupTypeTargetKeyPressHandling();
-    setupDimissTagListener();
-});
-
-
-
-/**
- *  When the fake textarea is clicked, pass focus to the hidden text-field.
- *  Also, bind to the focus and blur events of the text-field to update the
- *  fake textarea appearance, hence simulating focus
- */
-function setupSimulatedFocus(){
-    var jq_tagBox = $("#tag_box");
-    var jq_typeTarget = $("#type_target");
-
-    jq_tagBox.click(function(event){
-        jq_typeTarget.focus();
-    });
-
-    jq_typeTarget.focus(function(event){
-        jq_tagBox.addClass("has_focus");
-    });
-    jq_typeTarget.blur(function(event){
-        jq_tagBox.removeClass("has_focus");
-    });
-}
-
-
-
-
-
-/**
- *
- */
-function setupTypeTargetKeyPressHandling(){
-    var jq_typeTarget = $("#type_target");
-
-    //handle input: create tag if key==(enter, tab, comma, space), adjust width
-    jq_typeTarget.keydown(function(event){
-        var key = event.which
-        if (key === 13 || key === 9 || key === 44 || key === 188 || key === 32){
-            event.preventDefault();
-            verifyAndCreateTag();
-        }
-        else
-            adjustTypeTargetWidth();
-    });
-
-    //adjust width on keyup as well, to cover any case (crossbrowser)
-    jq_typeTarget.keyup(function(event){
-        adjustTypeTargetWidth();
-        //searchSuggestions();
-    });
-}
-
-
-
-
-/**
- *  
- */
-function setupDimissTagListener() {
-    $("#tag_box").on("click",
-                     ".tag_element > .tag_dismiss",
-                     null,
-                     function(event){
-                        event.preventDefault();
-                        removeTagElementAndHiddenInput($(this).parent());
-                     });
-}
-
-
-
-
-
-
-/**
- *  to adjust the hidden input's width
- */
-function adjustTypeTargetWidth() {
-    var jq_typeTarget = $("#type_target");
-    var len = jq_typeTarget.val().length;
-
-    jq_typeTarget.css("width", len*11);
-
-    if(parseInt(jq_typeTarget.css("width")) < 40){
-        jq_typeTarget.css("width", "40px")
-    }
-}
-
-
-
-
-/**
- *  if something has been typed in the input, create a new tag
- */
-function verifyAndCreateTag() {
-    var jq_typeTarget = $("#type_target");
-    var text = jq_typeTarget.val();
-
-    if(text.length > 0)
-        insertTagElement(text);
-}
-
-
-
-
-/**
- *  
- */
-function insertTagElement(text) {
-    var jq_typeTarget = $("#type_target");
-
-    var jq_newTag = $(buildTagHtmlString(text));
-    jq_newTag.insertBefore(jq_typeTarget);
-
-    // resetting the input
-    jq_typeTarget.val("").css("width", "60px");
+(function ($) {
  
-    insertTagHiddenField(text);
-}
+    var theForm;
+    var theBox;
+    var theTypeTarget;
 
+    $.fn.tag_box = function() {
+        theBox = this;
+        theForm = theBox.parents("form");
+        theTypeTarget = setupTypeTarget();
 
-/**
- *  
- */
-function insertTagHiddenField(text) {
-    jq_newField = $(buildHiddenInputHtmlString(text));
-    $("#the_form").prepend(jq_newField);
-}
+        setupTypeTargetKeyPressHandling();
 
+        setupExampleTagElement();
+        setupSimulatedFocus();
+        setupDimissTagListener();
 
-
-
-
-
-/**
- *  builds the html string for the tag element
- */
-function buildTagHtmlString(text) {
-    var tag_str = "<div class='tag_element'><span class='tag_label'>" + text +
-        "</span><a href='#' title='remove' class='tag_dismiss'>&times;</a></div>";
-    return tag_str;
-}
-
-
-/**
- *  builds the html string for a hidden form input
- */
-function buildHiddenInputHtmlString(text) {
-    var input_str =  "<input name='tag_list[]' type='hidden' value='" + text + "'>";
-    return input_str;
-}
+        return this;
+    };
 
 
 
-/**
- *  
- */
-function removeTagElementAndHiddenInput(jq_tag){
-    var tagName = jq_tag.find(".tag_label").html();
-    // remove the tag from the tag box
-    jq_tag.remove();
 
-    // remove the hidden input
-    var sel = '[value="' + tagName + '"]';
-    $(sel).remove();
-}
+    /**
+     *
+     */
+    function setupExampleTagElement(){
+        var exampleTagElement = $(buildExampleTagElement());
+        theBox.prepend(exampleTagElement);
+    }
+
+
+    /**
+     *  When the fake textarea is clicked, pass focus to the hidden text-field.
+     *  Also, bind to the focus and blur events of the text-field to update the
+     *  fake textarea appearance, hence simulating focus
+     */
+    function setupSimulatedFocus(){
+        theBox.click(function(event){
+            theTypeTarget.focus();
+        });
+        theTypeTarget.focus(function(event){
+            theBox.addClass("has_focus");
+        });
+        theTypeTarget.blur(function(event){
+            theBox.removeClass("has_focus");
+        });
+    }
+
+
+
+
+
+
+
+    /**
+     *
+     */
+    function setupTypeTarget(){
+        var typeTarget = $(buildTypeTarget());
+        theBox.append(typeTarget);
+
+        return typeTarget;
+    }
+    /**
+     *
+     */
+    function setupTypeTargetKeyPressHandling(){
+        //handle input: create tag if key==(enter, tab, comma, space), adjust width
+        theTypeTarget.keydown(function(event){
+            var key = event.which
+            if (key === 13 || key === 9 || key === 44 || key === 188 || key === 32){
+                event.preventDefault();
+                verifyAndCreateTag();
+            }
+            else
+                adjustTypeTargetWidth();
+        });
+
+        //adjust width on keyup as well, to cover any case (crossbrowser)
+        theTypeTarget.keyup(function(event){
+            adjustTypeTargetWidth();
+            //searchSuggestions();
+        });
+    }
+
+
+
+
+    /**
+     *  
+     */
+    function setupDimissTagListener() {
+        theBox.on("click",
+                 ".tag_element > .tag_dismiss",
+                 null,
+                 function(event){
+                    event.preventDefault();
+                    var theTagElement = $(this).parent();
+                    removeTagElementAndHiddenInput(theTagElement);
+                 });
+    }
+
+
+
+
+
+
+    /**
+     *  to adjust the hidden input's width
+     */
+    function adjustTypeTargetWidth() {
+        var len = theTypeTarget.val().length;
+
+        theTypeTarget.css("width", len*11);
+
+        if(parseInt(theTypeTarget.css("width")) < 40){
+            theTypeTarget.css("width", "40px")
+        }
+    }
+
+
+
+
+    /**
+     *  if something has been typed in the input, create a new tag
+     */
+    function verifyAndCreateTag() {
+        var text = theTypeTarget.val();
+
+        if(text.length > 0)
+            insertTagElement(text);
+    }
+
+
+
+
+    /**
+     *  
+     */
+    function insertTagElement(text) {
+        var jq_newTag = $(buildTagHtmlString(text));
+        jq_newTag.insertBefore(theTypeTarget);
+
+        // resetting the input
+        theTypeTarget.val("").css("width", "60px");
+     
+        insertTagHiddenField(text);
+    }
+
+
+    /**
+     *  
+     */
+    function insertTagHiddenField(text) {
+        jq_newField = $(buildHiddenInputHtmlString(text));
+        theForm.prepend(jq_newField);
+    }
+
+
+
+
+
+
+    /**
+     *  builds the html string for the tag element
+     */
+    function buildTagHtmlString(text) {
+        var tag_str = "<div class='tag_element'><span class='tag_label'>" + text +
+            "</span><a href='#' title='remove' class='tag_dismiss'>&times;</a></div>";
+        return tag_str;
+    }
+
+
+    /**
+     *  builds the html string for a hidden form input
+     */
+    function buildHiddenInputHtmlString(text) {
+        var input_str =  "<input name='tag_list[]' type='hidden' value='" + text + "'>";
+        return input_str;
+    }
+
+
+
+    /**
+     *  
+     */
+    function removeTagElementAndHiddenInput(jq_tag){
+        var tagName = jq_tag.find(".tag_label").html();
+        // remove the tag from the tag box
+        jq_tag.remove();
+
+        // remove the hidden input
+        var sel = '[value="' + tagName + '"]';
+        $(sel).remove();
+    }
+
+
+    /**
+     *  builds the html string for the example tag
+     */
+    function buildExampleTagElement() {
+        var tagStr = '<div class="tag_element"><span class="tag_label">example</span><a href="#" title="remove" class="tag_dismiss">&times;</a></div>';
+        return tagStr;
+    }
+
+
+    /**
+     *  builds the html string for the example tag
+     */
+    function buildTypeTarget() {
+        var tagStr = '<input name="type_target" id="type_target" type="text" maxlength="40">';
+        return tagStr;
+    }
+
+    
+}(jQuery));
+
+
+
+
 
 
 
